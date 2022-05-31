705db79789f5f114f75f7e67e9bd380ea33f2d52
thdwlgh44 <thdwlgh44@naver.com>
2022-05-31 18:38:05 +0900

    second commit

--- Board/src/main/java/com/board/BoardApplication.java
+++ Board/src/main/java/com/board/BoardApplication.java
@@ -3,6 +3,7 @@ package com.board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//스프링 부트 어플
 @SpringBootApplication
 public class BoardApplication {
 
---
+++ Board/src/main/java/com/board/adapter/Adapter.java
@@ -0,0 +1,5 @@
+package com.board.adapter;
+
+public class Adapter {
+
+}
