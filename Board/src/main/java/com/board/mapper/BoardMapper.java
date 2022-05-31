823d7b7afa174cb0a3b9161b564f662438813c02
thdwlgh44 <thdwlgh44@naver.com>
2022-05-31 18:32:28 +0900

    first commit

---
+++ Board/src/main/java/com/board/mapper/BoardMapper.java
@@ -0,0 +1,25 @@
+package com.board.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.board.domain.BoardDTO;
+import com.board.paging.Criteria;
+
+@Mapper
+public interface BoardMapper {
+
+	public int insertBoard(BoardDTO params);
+
+	public BoardDTO selectBoardDetail(Long idx);
+
+	public int updateBoard(BoardDTO params);
+
+	public int deleteBoard(Long idx);
+
+	public List<BoardDTO> selectBoardList(BoardDTO params);
+
+	public int selectBoardTotalCount(BoardDTO params);
+
+}
---
+++ Board/src/test/java/com/board/MapperTests.java
@@ -0,0 +1,50 @@
+package com.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.board.domain.BoardDTO;
+import com.board.mapper.BoardMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+@SpringBootTest
+class MapperTests {
+
+	@Autowired
+	private BoardMapper boardMapper;
+
+	@Test
+	public void testOfInsert() {
+		BoardDTO params = new BoardDTO();
+		params.setTitle("1번 게시글 제목");
+		params.setContent("1번 게시글 내용");
+		params.setWriter("테스터");
+		System.out.println(params);
+
+		
+		int result = boardMapper.insertBoard(params);
+		System.out.println("결과는 " + result + "입니다.");
+	}
+	
+	@Test
+	public void testOfSelectDetail() {
+		BoardDTO board = boardMapper.selectBoardDetail((long) 1);
+		try {
+			//String boardJson = new ObjectMapper().writeValueAsString(board);
+            String boardJson = new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(board);
+
+			System.out.println("=========================");
+			System.out.println(boardJson);
+			System.out.println("=========================");
+
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
